be0625948ffa4d7e43e51bb6813aedb798146751
5Suminsss2 <sumin.kim.dev@gmail.com>
2023-04-12 16:12:05 +0900

    Chapter3. 이미지 구현하기

--- front/components/PostImages.js
+++ front/components/PostImages.js
@@ -1,11 +1,89 @@
+import React, { useCallback, useState } from "react";
 import PropTypes from "prop-types";
+import { PlusOutlined } from "@ant-design/icons";
 
 const PostImages = ({ images }) => {
-  return <>구현중</>;
+  const [showImagesZoom, setShowImagesZoom] = useState(false);
+
+  const onZoom = useCallback(() => {
+    setShowImagesZoom(true);
+  }, []);
+
+  const onClose = useCallback(() => {
+    setShowImagesZoom(false);
+  }, []);
+
+  if (images.length === 1) {
+    return (
+      <>
+        <img
+          role="presentation"
+          src={images[0].src}
+          alt={images[0].src}
+          onClick={onZoom}
+        />
+        {/*  */}
+      </>
+    );
+  }
+  if (images.length === 2) {
+    return (
+      <>
+        <div>
+          <img
+            role="presentation"
+            src={images[0].src}
+            alt={images[0].src}
+            width="50%"
+            onClick={onZoom}
+          />
+          <img
+            role="presentation"
+            src={images[1].src}
+            alt={images[1].src}
+            width="50%"
+            onClick={onZoom}
+          />
+        </div>
+      </>
+    );
+  }
+  return (
+    <>
+      <div>
+        <img
+          role="presentation"
+          src={images[0].src}
+          alt={images[0].src}
+          width="50%"
+          onClick={onZoom}
+        />
+        <div
+          role="presentation"
+          style={{
+            display: "inline-block",
+            width: "50%",
+            textAlign: "center",
+            verticalAlign: "middle",
+          }}
+          onClick={onZoom}
+        >
+          <PlusOutlined />
+          <br />
+          {images.length - 1}
+          개의 사진 더보기
+        </div>
+      </div>
+    </>
+  );
 };
 
-PostImages.prototype = {
-  images: PropTypes.arrayOf(PropTypes.object),
+PostImages.propTypes = {
+  images: PropTypes.arrayOf(
+    PropTypes.shape({
+      src: PropTypes.string,
+    })
+  ).isRequired,
 };
 
 export default PostImages;
