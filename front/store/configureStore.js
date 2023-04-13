e1d7afeb1284397fdb15f733b4b0ca6b8370ebab
5Suminsss2 <sumin.kim.dev@gmail.com>
2023-04-13 13:31:29 +0900

    Chpater4. redux thunk

--- front/store/configureStore.js
+++ front/store/configureStore.js
@@ -2,19 +2,27 @@ import { createWrapper } from "next-redux-wrapper";
 import { applyMiddleware, compose, createStore } from "redux";
 import reducer from "../reducers";
 import { composeWithDevTools } from "redux-devtools-extension";
+import thunkMiddleware from "redux-thunk";
+
+// thunk : 리덕스를 비동기로 실행할 수 있게 해주는 미들웨어
+// thunk 장점 : 하나의 비동기 액션 안에서 여러개의 동기 액션을 넣을 수 있음/  dispatch를 여러번 할 수 있음
+
+const loggerMiddleware =
+  ({ dispatch, getState }) =>
+  (next) =>
+  (action) => {
+    console.log(action); // 액션을 실행학시 전에 콘솔창 띄우는 미들웨어
+    return next(action);
+  };
 
 const configureStore = () => {
-  const middlewares = [];
+  const middlewares = [thunkMiddleware, loggerMiddleware]; // 미들웨어 : 리덕스의 기능 향상시키기
   const enhancer =
     process.env.NODE_ENV === "production"
       ? compose(applyMiddleware(...middlewares))
       : composeWithDevTools(applyMiddleware(...middlewares)); // redux dev tools와 연동하기
   const store = createStore(reducer, enhancer);
 
-  store.dispatch({
-    type: "CHANGE_NICKNAME",
-    data: "boogicho",
-  });
   return store;
 };
 
