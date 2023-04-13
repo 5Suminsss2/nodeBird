2028776c6f3cf69bd9a473b49101e83323920f2f
5Suminsss2 <sumin.kim.dev@gmail.com>
2023-04-13 13:31:06 +0900

    Chpater4. redux thunk

--- front/reducers/user.js
+++ front/reducers/user.js
@@ -5,17 +5,42 @@ export const initialState = {
   loginData: {},
 };
 
-// action creator
 export const loginAction = (data) => {
+  // THUNK 장점 : 한번에 dispatch가 여러개!!
+  // THUNK 단점 : Delay가 같은 사가가 할 수 있는 것들은 다 구현해야함
+  return (dispatch, getState) => {
+    const state = getState(); // initial Reducer를 가져옴
+
+    dispatch(logoinRequestAction());
+    axios
+      .post("/api/login")
+      .then((res) => {
+        dispatch(logoinSuccessAction(res.data));
+      })
+      .catch((err) => {
+        dispatch(loginFailureAction(err));
+      });
+  };
+};
+
+// action creator
+export const logoinRequestAction = (data) => {
   return {
-    type: "LOG_IN",
+    type: "LOG_IN_REQUEST",
     data,
   };
 };
 
-export const logoutAction = (data) => {
+export const logoinSuccessAction = (data) => {
   return {
-    type: "LOG_OUT",
+    type: "LOG_IN_SUCCESS",
+    data,
+  };
+};
+
+export const loginFailureAction = (data) => {
+  return {
+    type: "LOG_OUT_FAILURE",
     data,
   };
 };
